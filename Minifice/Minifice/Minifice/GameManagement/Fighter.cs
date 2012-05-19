14f0d58a177f0d6d20f10d2cb820c7965eaeab3c
kelostrada@gmail.com <kelostrada@gmail.com@e57b8afe-0566-8d80-358a-9211f2cfe894>
2012-05-19 19:52:50 +0000

    Poprawki do chodzenia. Jeszcze troszkę trzeba poprawić, ale głównie w boundaries obiektów, bo na nich się zacina najbardziej.

--- Minifice/Minifice/Minifice/GameManagement/Fighter.cs
+++ Minifice/Minifice/Minifice/GameManagement/Fighter.cs
@@ -35,16 +35,17 @@ namespace Minifice.GameManagement
 
         #region Inicjalizacja
 
-        public Fighter(bool playerControlled)
+        public Fighter(bool playerControlled, Vector2 position)
         {
             this.playerControlled = playerControlled;
+            this.position = position;
 
             // Definicja animacji postaci
             this.animation = new Animation(2, 5, Direction.Left);
             AnimationFrame mini1 = new AnimationFrame(@"Game\mini1", new Rectangle(0, 0, 200, 200), new Rectangle(0, 0, 40, 40));
-            mini1.origin = new Vector2(100, 160);
+            mini1.origin = new Vector2(100, 180);
             AnimationFrame mini2 = new AnimationFrame(@"Game\mini2", new Rectangle(0, 0, 200, 200), new Rectangle(0, 0, 40, 40));
-            mini2.origin = new Vector2(100, 160);
+            mini2.origin = new Vector2(100, 180);
             this.animation.framesLeft.Add(mini1);
             this.animation.framesLeft.Add(mini2);
             this.animation.framesRight.Add(mini1);
@@ -57,17 +58,16 @@ namespace Minifice.GameManagement
             this.animationDeath = new Animation();
 
             List<Vector2> points = new List<Vector2>();
-            points.Add(new Vector2(-10, -10));
-            points.Add(new Vector2(-10, 10));
-            points.Add(new Vector2(10, 10));
-            points.Add(new Vector2(10, -10));
+            points.Add(new Vector2(-7, 0));
+            points.Add(new Vector2(0, -7));
+            points.Add(new Vector2(7, 0));
+            points.Add(new Vector2(0, 7));
 
             this.boundaries = Boundaries.CreateFromPoints(points);
             
             this.health = 2;
             this.isActive = true;
-            this.position = new Vector2(100f, 100f);
-            this.speed = 1.1f;
+            this.speed = 0.7f;
             this.timeLastShot = new TimeSpan();
         }
 
@@ -79,24 +79,6 @@ namespace Minifice.GameManagement
         {
             if (playerControlled)
             {
-
-                /*
-                Przelicz pozycję myszy na współrzędne z mapy; (CalculateCoord)
-    Oblicz odległość od punktu docelowego;
-    // Oblicz drogę jaką przeszła postać w czasie od ostatniego przesunięcia:
-    s = prędkość postaci * (czas teraz – czas ostatniego przesunięcia);
-    Normalizuj wektor pozycji gracza względem pozycji myszy;
-    Przesuń skopiowana pozycje postaci o s * znormalizowany wektor;
-    Dla każdego przeciwnika, wojownika i obiektu mapy
-    {
-    15
-    Sprawdź czy nie zachodzi kolizja; if (koliduje z bonusem)
-    Dodaj Bonus;
-    }
-    if (nie zachodzi kolizja)
-    Przesuń postać(wektor);
-    Animuj ruch(kierunek);
-                */
                 Vector2 posClick = new Vector2(input.CurrentMouseState.X - GameInterface.Width - (resolution.X - GameInterface.Width) / 2, input.CurrentMouseState.Y - resolution.Y/2 );
 
                 float s = speed * (currentTime.ElapsedGameTime.Ticks) * 0.00001f;
@@ -109,34 +91,11 @@ namespace Minifice.GameManagement
                 Vector2 oldPosition = position;
                 position += posClick;
 
-                // TODO: Sprawdzenie kolizji
-                int j = (int)Math.Floor((2 * (int)position.Y) / GameMap.TileShift.Y);
-                int i = (int)Math.Floor((int)position.X / GameMap.TileShift.X - ((j % 2 == 1) ? 1 / 2 : 0));
-                if (i < 0) i = 0;
-                if (i > gameMap.width - 1) i = gameMap.width - 1;
-                if (j < 0) j = 0;
-                if (j > gameMap.height - 1) j = gameMap.height - 1;
 
-                bool intersects = false;
-
-                for (int k = -3; k < 4; k++)
-                {
-                    for (int l = -3; l < 4; l++)
-                    {
-                        if (i + k >= 0 && j + l >= 0 && i + k < gameMap.width - 1 && j + l < gameMap.height - 1)
-                        {
-                            foreach (var mo in gameMap.mapTiles[i + k][j + l].mapObjects)
-                            {
-                                
-                                if (mo.boundaries.Intersects(boundaries+position)) intersects = true;
-                            }
-                        }
-                    }
-                }
 
                 Direction direction=animation.direction;
 
-                if (!intersects)
+                if (!Collision(gameMap, fighters, enemies))
                 {
                     // Kierunek animacji
                     Vector2 kier = position - oldPosition;
@@ -158,11 +117,95 @@ namespace Minifice.GameManagement
                 else
                 {
                     position -= posClick;
+                    
+                    // Helper do poruszania sie wzdłuż boundaries?
+                    float degrees = 5;
+                    while (degrees < 90)
+                    {
+                        degrees *= -1;
+                        degrees += Math.Sign(degrees) * 5;
+
+                        posClick = Vector2.Transform(posClick, Matrix.CreateRotationZ(MathHelper.ToRadians(degrees)));
+
+                        position += posClick;
+
+                        if (!Collision(gameMap, fighters, enemies))
+                        {
+                            // Kierunek animacji
+                            Vector2 kier = position - oldPosition;
+                            if (kier.X > 0)
+                            {
+                                if (kier.X > Math.Abs(kier.Y))
+                                    direction = Direction.Right;
+                                else
+                                    direction = (kier.Y > 0) ? Direction.Down : Direction.Up;
+                            }
+                            else
+                            {
+                                if (Math.Abs(kier.X) > Math.Abs(kier.Y))
+                                    direction = Direction.Left;
+                                else
+                                    direction = (kier.Y > 0) ? Direction.Down : Direction.Up;
+                            }
+                            degrees = 100;
+                        }
+                        else
+                        {
+                            position -= posClick;
+                        }
+                     
+                    }
+                    
+
+
                 }
                 animation.Update(direction, currentTime);
                 return position;
             }
             return null;
+        }
+
+        private bool Collision(GameMap gameMap, List<Fighter> fighters, List<Enemy> enemies)
+        {
+            // Sprawdzenie kolizji
+            int j = (int)Math.Floor((2 * (int)position.Y) / GameMap.TileShift.Y);
+            int i = (int)Math.Floor((int)position.X / GameMap.TileShift.X - ((j % 2 == 1) ? 1 / 2 : 0));
+            if (i < 0) i = 0;
+            if (i > gameMap.width) i = gameMap.width - 1;
+            if (j < 0) j = 0;
+            if (j > gameMap.height - 1) j = gameMap.height - 1;
+
+            bool intersects = false;
+
+            for (int k = -3; k < 4; k++)
+            {
+                for (int l = -3; l < 4; l++)
+                {
+                    if (i + k >= 0 && j + l >= 0 && i + k < gameMap.width && j + l < gameMap.height)
+                    {
+                        foreach (var mo in gameMap.mapTiles[i + k][j + l].mapObjects)
+                        {
+                            if ((j + l) % 2 == 0)
+                            {
+                                if ((mo.boundaries + new Vector2((i + k) * GameMap.TileShift.X, (j + l) * GameMap.TileShift.Y / 2)).Intersects(boundaries + position)) intersects = true;
+                            }
+                            else
+                            {
+                                if ((mo.boundaries + new Vector2((i + k) * GameMap.TileShift.X + GameMap.TileShift.X / 2, (j + l) * GameMap.TileShift.Y / 2)).Intersects(boundaries + position)) intersects = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (Fighter f in fighters)
+            {
+                if (!f.Equals(this))
+                    if ((boundaries + position).Intersects(f.boundaries + f.position))
+                        intersects = true;
+            }
+
+            return intersects;
         }
 
         public override void Move(GameMap gameMap, List<Fighter> fighters, List<Enemy> enemies)
@@ -178,7 +221,7 @@ namespace Minifice.GameManagement
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
 
-            animation.Draw(spriteBatch, position, position.Y /(GameMap.TileShift.Y / 2) * 0.001f + 0.002f);
+            animation.Draw(spriteBatch, position, position.Y /(GameMap.TileShift.Y / 2) * 0.001f + 0.001f);
         }
 
         #endregion
